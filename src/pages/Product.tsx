4a80a1cd546a04168bd6e3fdaa7509da93a85341
hamedesmaailii <hamed.esmaili.1375@gmail.com>
2025-03-18 12:44:06 +0330

    Add Product Page

---
+++ src/pages/Product.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+import Breadcrumb from "../components/Breadcrumb";
+import ProductsSection from "../components/ProductsSection";
+
+const Product = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Product;
