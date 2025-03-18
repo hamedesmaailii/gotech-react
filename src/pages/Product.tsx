55da16d9d9b96cece86d1bbd50049799733a30be
hamedesmaailii <hamed.esmaili.1375@gmail.com>
2025-03-18 12:48:28 +0330

    Update Product Page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,11 +1,27 @@
-import React from "react";
 import Header from "../components/Header";
 import Breadcrumb from "../components/Breadcrumb";
 import ProductsSection from "../components/ProductsSection";
 
 const Product = () => {
     return (
-        <div></div>
+        <div>
+            <div className="container mx-auto">
+                <Header />
+            </div>
+            <Breadcrumb
+                title="All Products"
+                items={[
+                    {label: "Home", path: '/'},
+                    {label: "Product"}
+                ]}
+            />
+            <div className="container mx-auto">
+                <ProductsSection
+                    title="Our Owning Products"
+                    subTitle="Lorem ipsum dolor sit amet, vel accumsan liberavisse ex, ea nec <br/> habemus sea ut. His nibh scripta in. In sea vocibus facilisis."
+                />
+            </div>
+        </div>
     )
 }
 
